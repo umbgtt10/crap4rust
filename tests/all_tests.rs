055df1ab8a359500572a47371a5a8a9e744068eb
Umberto Gotti <umberto.gotti@umbertogotti.dev>
2026-04-27 21:57:24 +0200

    Restructure tests: split into fixture_tests + app_tests, add all_tests plumbing

---
+++ tests/all_tests.rs
@@ -0,0 +1,6 @@
+// Copyright 2025 Umberto Gotti <umberto.gotti@umbertogotti.dev>
+// Licensed under the MIT License or Apache License, Version 2.0
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub mod app_tests;
+pub mod fixture_tests;
